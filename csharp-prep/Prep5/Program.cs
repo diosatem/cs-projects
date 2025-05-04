35188d666960c13fc1d6e7fa78fbc9adf610dd9d
diosatem <jam19021@byui.edu>
2025-05-04 21:07:11 +0800

    Finished Prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,59 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        //Task 1
+        DisplayWelcome();
+
+        //Task 2
+        Console.Write("What is your name? ");
+        string name = Console.ReadLine();
+        PromptUserName(name);
+
+        //Task 3
+        Console.Write("What is your favorite number? ");
+        string userInput = Console.ReadLine();
+        int faveNum = int.Parse(userInput);
+        PromptUserNumber(faveNum);
+
+        //Task 4
+        SquareNumber(faveNum);
+
+        //Task 5
+        DisplayResult(name, faveNum);
+    }
+
+    //Task 1
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    //Task 2
+    static string PromptUserName(string userName)
+    {
+string inputtedName = userName;     
+return inputtedName;   
+    }
+
+    //Task 3
+    static int PromptUserNumber(int faveNumber)
+    {
+        int inputtedNumber = faveNumber;
+        return inputtedNumber;
+    }
+
+    //Task 4
+    static double SquareNumber(int faveNum)
+    {
+        double squared = Math.Pow(faveNum, 2);
+        return squared;
+    }
+
+    //Task 5
+    static void DisplayResult(string inputtedName, int squared)
+    {
+        string displayName = PromptUserName(inputtedName);
+        double displayNumber = SquareNumber(squared);
+        Console.WriteLine($"{displayName}, the square of your favorite number is {displayNumber}.");
     }
 }

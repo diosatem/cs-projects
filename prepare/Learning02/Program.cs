62f0b199b79bbf12b4fc76158f5f8758f6c9163b
diosatem <jam19021@byui.edu>
2025-05-10 03:50:14 +0800

    Finished Learning 02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        // Console.WriteLine("Hello Learning02 World!");
+
+        Job job1 = new Job();
+        job1._company = "NextWebb Website Solutions";
+        job1._jobTitle = "Web Developer";
+        job1._startYear = 2011;
+        job1._endYear = 2019;
+
+        // Console.WriteLine(job1._company);
+
+        Job job2 = new Job();
+        job2._company = "Rend'r Creative Pty Ltd";
+        job2._jobTitle = "Dev Engineer";
+        job2._startYear = 2023;
+        job2._endYear = 2025;
+
+        // Console.WriteLine(job2._company);
+
+
+        // job1.DisplayJobDetails();
+        // job2.DisplayJobDetails();
+
+        Resume resume = new Resume();
+        resume._name = "Shoi Joy";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        // Console.WriteLine(resume._jobs[1]._jobTitle);
+
+        resume.DisplayResume();
     }
 }

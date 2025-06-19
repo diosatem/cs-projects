611588700d6b9c236de45caef506657dcf2eab39
diosatem <jam19021@byui.edu>
2025-06-20 01:39:18 +0800

    Finished L05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("purple", 10);
+        // Console.WriteLine($"Square color: {square.GetColor()}");
+        // Console.WriteLine($"Square area: {square.GetArea()}");
+
+        Rectangle rectangle = new Rectangle("yellow", 10, 5);
+        // Console.WriteLine($"Rectangle color: {rectangle.GetColor()}");
+        // Console.WriteLine($"Rectangle color: {rectangle.GetArea()}");
+
+        Circle circle = new Circle("red", 20);
+        // Console.WriteLine($"Circle color: {circle.GetColor()}");
+        // Console.WriteLine($"Circle color: {circle.GetArea()}");
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape s in shapes)
+        {
+            Console.WriteLine($"Color: {s.GetColor()}");
+            Console.WriteLine($"Area: {s.GetArea()}");
+        }
     }
 }
